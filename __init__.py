769784bbebeaa94a1d1a4d5ee22bd9d089ad7c09
Gius <112352961+GiusTex@users.noreply.github.com>
2026-01-29 12:03:30 +0100

    add SamplerCustomUltraAdvancedPlusEfficient class

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
 from .samplers import (SamplerCustomAdvanced_Efficient,
-                       SamplerCustomUltraAdvancedEfficient)
+                       SamplerCustomUltraAdvancedEfficient,
+                       SamplerCustomUltraAdvancedPlusEfficient)
 
 
 NODE_CLASS_MAPPINGS = {
     "SamplerCustomAdvanced_Efficient": SamplerCustomAdvanced_Efficient,
     "SamplerCustomUltraAdvancedEfficient": SamplerCustomUltraAdvancedEfficient,
+    "SamplerCustomUltraAdvancedPlusEfficient": SamplerCustomUltraAdvancedPlusEfficient,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SamplerCustomAdvanced_Efficient": "Sampler Custom Advanced (Efficient)",
     "SamplerCustomUltraAdvancedEfficient": "Sampler Custom Ultra Advanced (Efficient)",
+    "SamplerCustomUltraAdvancedPlusEfficient": "Sampler Custom Ultra Advanced Plus (Efficient)",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
