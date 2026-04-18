5b37146fb01364d8889068c252fbd3fd2c4a1e1e
Gius <112352961+GiusTex@users.noreply.github.com>
2026-04-18 19:01:24 +0200

    update init

--- __init__.py
+++ __init__.py
@@ -1,18 +1,14 @@
-from .samplers import (SamplerCustomAdvanced_Efficient,
-                       SamplerCustomUltraAdvancedEfficient,
-                       SamplerCustomUltraAdvancedPlusEfficient)
-
+from .samplers import (
+    NODE_CLASS_MAPPINGS as NODE_CLASS_MAPPINGS_samplers, 
+    NODE_DISPLAY_NAME_MAPPINGS as NODE_DISPLAY_NAME_MAPPINGS_samplers
+    )
 
 NODE_CLASS_MAPPINGS = {
-    "SamplerCustomAdvanced_Efficient": SamplerCustomAdvanced_Efficient,
-    "SamplerCustomUltraAdvancedEfficient": SamplerCustomUltraAdvancedEfficient,
-    "SamplerCustomUltraAdvancedPlusEfficient": SamplerCustomUltraAdvancedPlusEfficient,
-}
+    **NODE_CLASS_MAPPINGS_samplers, 
+    }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SamplerCustomAdvanced_Efficient": "Sampler Custom Advanced (Efficient)",
-    "SamplerCustomUltraAdvancedEfficient": "Sampler Custom Ultra Advanced (Efficient)",
-    "SamplerCustomUltraAdvancedPlusEfficient": "Sampler Custom Ultra Advanced Plus (Efficient)",
-}
+    **NODE_DISPLAY_NAME_MAPPINGS_samplers,
+    }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
